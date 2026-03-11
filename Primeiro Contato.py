0a9b9bc1862b86b3a843e8d12ff7b04aff61be1d
Rafael <rafael.audibert@senior.com.br>
2026-03-11 17:33:50 -0300

    Adicionando pagina primeiro contato

--- Primeiro Contato.py
+++ Primeiro Contato.py
@@ -7,7 +7,7 @@ st.title("📊 Portal Torre ERP")
 st.write("Bem-vindo ao portal.")
 st.write("Aqui você encontrará tudo sobre a Torre ERP.")
 
-st.subheader("📄 Documentação")
+st.subheader("📄 Documentações")
 
 # Link clicável e bonito
 st.markdown(
@@ -16,6 +16,3 @@ st.markdown(
     [Clique aqui para abrir o documento](https://seniorsistemassa.sharepoint.com/sites/SGPinicial/SitePages/PO250.aspx?csf=1&web=1&e=aqMc6F&CID=38dd7acc-1564-455a-8e0b-941daf733808)
     """
 )
-
-# Imagem PNG abaixo
-st.image("squadFoundation.png", caption="Portal Torre ERP", use_container_width=True)
---
+++ pages/Times do ERP.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Nossos Times", page_icon="📊", layout="centered")
+
+# Imagem PNG abaixo
+st.image("squadFoundation.png", caption="Squard Foundation ERP", use_container_width=True)
