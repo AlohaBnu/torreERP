219ee7fe4f3b50b485f7c6ca6c0e760fba5f4c92
Rafael <rafael.audibert@senior.com.br>
2026-03-11 17:39:42 -0300

    Adicionando pagina primeiro contato

--- pages/Times do ERP.py
+++ pages/Times do ERP.py
@@ -4,3 +4,6 @@ st.set_page_config(page_title="Nossos Times", page_icon="📊", layout="centered
 
 # Imagem PNG abaixo
 st.image("squadFoundation.png", caption="Squad Foundation ERP", use_container_width=True)
+
+# Imagem PNG abaixo
+st.image("TechLead.png", caption="Squad Tech Lead ERP", use_container_width=True)
