4693ca1af29c2ba194f9504aa6be12bb9b64356f
Rafael <rafael.audibert@senior.com.br>
2026-03-11 17:35:05 -0300

    Adicionando pagina primeiro contato

--- pages/Times do ERP.py
+++ pages/Times do ERP.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.set_page_config(page_title="Nossos Times", page_icon="📊", layout="centered")
 
 # Imagem PNG abaixo
-st.image("squadFoundation.png", caption="Squard Foundation ERP", use_container_width=True)
+st.image("squadFoundation.png", caption="Squad Foundation ERP", use_container_width=True)
