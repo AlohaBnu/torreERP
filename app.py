c30f0399f741bf3dcdbe1e9a6fe295c013a19d76
Rafael <rafael.audibert@senior.com.br>
2026-03-11 15:00:42 -0300

    Adicionando pagina primeiro contato

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Portal Torre ERP")
+
+st.title("Portal Torre ERP")
+st.write("Bem-vindo ao portal.")
+st.write("Use o menu lateral para navegar.")
